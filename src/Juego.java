24c34171737af477f347388d1c4610a07852a161
Paulo <pescanellasotero@gmail.com>
2025-04-01 12:34:19 +0200

    Finalizamos el proyecto implementando todo el codigo necesario para realizar una partida en la clase Juego, la cual llamaremos desde el main para iniciar una partida

--- src/Juego.java
+++ src/Juego.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class Juego {
 
     public void iniciarJuego(){
 
+        Scanner sc = new Scanner(System.in);
+
+        Tablero tablero = new Tablero();
+        System.out.print("Introduce tu nombre: ");
+        Jugador jugador = new Jugador(sc.next());
+        sc.nextLine();
+
+        tablero.iniciarBarcos();
+        tablero.colocarBarcos();
+
+        boolean partida = true;
+
+        do{
+            tablero.imprimirTablero();
+
+            System.out.println(jugador.getNombre() + ", establece las coordenadas:");
+
+            System.out.print("Fila (0-9): ");
+            int fila = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Columna (0-9): ");
+            int columna = sc.nextInt();
+            sc.nextLine();
+
+            tablero.tocarBarco(fila, columna);
+
+            if(tablero.victoria()){
+                partida = false;
+            }
+
+        }while (partida);
     }
 }
